e0ceee69955aecd37c63324c5e53ad08888d4443
Aragas <micvitalij@yandex.ru>
2017-06-29 16:35:44 +0300

    Added .Net Core projects

--- Open.Nat.UnitTests/UpnpNatDeviceInfoTests.cs
+++ Open.Nat.UnitTests/UpnpNatDeviceInfoTests.cs
@@ -7,11 +7,13 @@ namespace Open.Nat.UnitTests
 	[TestClass]
 	public class UpnpNatDeviceInfoTests
 	{
-		[TestMethod]
+#if !NETCOREAPP2_0
+        [TestMethod]
 		public void x()
 		{
 			var info = new UpnpNatDeviceInfo(IPAddress.Loopback, new Uri("http://127.0.0.1:3221"), "/control?WANIPConnection", null);
 			Assert.AreEqual("http://127.0.0.1:3221/control?WANIPConnection", info.ServiceControlUri.ToString());
 		}
-	}
+#endif
+    }
 }
--- Open.Nat/AssemblyInfo.cs
+++ Open.Nat/AssemblyInfo.cs
@@ -3,11 +3,19 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+#if NETSTANDARD2_0
+[assembly: AssemblyTitle("Open.Nat.Core")]
+#else
 [assembly: AssemblyTitle("Open.Nat")]
+#endif
 [assembly: AssemblyDescription(".NET Library for automatic network address translation")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+#if NETSTANDARD2_0
+[assembly: AssemblyProduct("Open.Nat.Core")]
+#else
 [assembly: AssemblyProduct("Open.Nat")]
+#endif
 [assembly: AssemblyCopyright("Copyright Alan McGovern, Ben Motmans, Lucas Ontivero ©  2006-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -16,9 +24,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2.1.0.0")]
 [assembly: AssemblyFileVersion("2.1.0.0")]
 [assembly: CLSCompliant(false)]
+#if !NETSTANDARD2_0
 [assembly: InternalsVisibleTo("Open.Nat.UnitTests,PublicKey=" + 
 	"00240000048000009400000006020000002400005253413100040000010001004de491ec95076f"+
 	"7054ac0562395232c4c351bce7037c7e266a5cc5dc6c60738fe649e06513ca1d58edec8e6a60e9"+
 	"69887403a09ae158a007a592f650fe98912e7029e112e61494a47eff16138b62390241e1d8d450"+
 	"76ac3af77b47fa67d40ba1928a37ebe073415785a23ae87b6119f63f78f04cb3b8ea280dfbf3d6"+
 	"df437fbf")]
+#endif
